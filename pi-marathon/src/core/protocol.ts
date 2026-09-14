418eb6c04f64e66def96e6f3b97e6822bc008a73
prospect-ogujiuba <prospect.ogujiuba@gmail.com>
2026-09-13 21:02:41 -0400

    remove pi-marathon

--- pi-marathon/src/core/protocol.ts
+++
@@ -1,4 +0,0 @@
-import type { MarathonConfig } from "./types.js";
-export interface CreateRunRequest { goal:string; config?:Partial<MarathonConfig> }
-export interface SteeringRequest { message:string }
-export interface BudgetPatchRequest { maxTasks?:number; maxAgentCalls?:number; maxRunMinutes?:number; maxCostUsd?:number|null; maxFinalAuditCycles?:number }
